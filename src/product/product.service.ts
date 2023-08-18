f305e20ac46bf7e580e954d92c41dccab8ca343d
kunal786ks <ks445kunalsharma@gmail.com>
2023-08-18 18:07:36 +0530

    qwr

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -10,7 +10,7 @@ export class ProductService {
     constructor(@InjectModel(product.name) private productModel:Model<product>){}
 
     async createProduct(createProductdto:CreateProductDto){
-        const newProduct=await new this.productModel(createProductdto).populate('owner')
+        const newProduct=await new this.productModel(createProductdto).populate('owner') //in populate we add the field in which we create the ref and in ref we paased the model name 
         return newProduct.save()
     }
 
